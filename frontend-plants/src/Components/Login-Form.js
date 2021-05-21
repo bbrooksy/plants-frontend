e9bf9e5549cf78591b6fb277d64e4c6e70ddc848
bbrooksy <72233725+bbrooksy@users.noreply.github.com>
2021-05-20 21:03:29 -0400

    initial form setup

---
+++ frontend-plants/src/Components/Login-Form.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+
+
+    export default function LoginForm() {
+        const initialValues = {user_name: "", password: ""}
+        const [logincreds, setLoginCreds] = useState(initialValues)
+        const handleLoginChange = e => {
+          setLoginCreds({
+            ...logincreds,
+            [e.target.name]: e.target.value
+            }
+          )} 
+          const handleSubmit = e => {
+            ///insert redux dispatch
+           }
+
+return (
+    <div className="login">
+      <form className='form' onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        <label>
+          Username:
+            <input 
+              name='user_name'
+              type='text'
+              autoFocus
+              value={logincreds.user_name}
+              onChange={handleLoginChange}
+              required/>
+        </label>
+          <br/>
+        <label>
+          Password:
+            <input
+              name='password'
+              type='password'
+              value={logincreds.password}
+              onChange={handleLoginChange}
+              required/>
+        </label>
+              <br/>
+            <CustomButton>Submit</CustomButton>
+        </form>
+      </div>
+    )
+  }
+  
